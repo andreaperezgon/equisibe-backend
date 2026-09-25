cd71a9b9e2456c748d74e230d726f9cbdbd42116
andreaperezgon <andreaperezgon@gmail.com>
2026-09-25 11:54:37 +0200

    feat: enable CORS for contact requests

--- src/main/java/com/equisibe/controller/ContactController.java
+++ src/main/java/com/equisibe/controller/ContactController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/contacts")
 public class ContactController {
